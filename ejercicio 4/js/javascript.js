0db20b96d8551754d9ddd2cdc5b6d301fe72829c
Jose Martinez <jose.martinez.mayora@gmail.com>
2017-10-27 23:04:11 -0300

    modification

---
+++ ejercicio 4/js/javascript.js
@@ -0,0 +1,10 @@
+
+  function suma (a, b) {
+
+   a = prompt("ingresa un número");
+   b = prompt("ingresa otro número");
+   c =parseInt(a) + parseInt(b);
+   return alert("la suma es " + c );
+   	
+    }
+suma();
